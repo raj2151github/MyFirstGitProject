de24879721560eafd7a81d021c3bf8ad7d81c55f
raj <rajthombare50@gmail.com>
2023-07-12 11:18:10 +0530

    first commit

---
+++ src/com/velocity/demo/Bank.java
@@ -0,0 +1,12 @@
+package com.velocity.demo;
+
+public class Bank {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("WELCOME TO SBI BANK");
+		
+		
+	}
+
+}
---
+++ src/com/velocity/demo/Example.java
@@ -0,0 +1,10 @@
+package com.velocity.demo;
+
+public class Example {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("welcome java program");
+	}
+
+}
